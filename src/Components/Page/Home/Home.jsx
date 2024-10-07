594d9571af136a4e02b6f4675b9f57b4feaa4ed0
Amar Kumar <amar2mail9@gmail.com>
2024-10-07 18:57:12 +0530

    Check Deplyoments

--- src/Components/Page/Home/Home.jsx
+++ src/Components/Page/Home/Home.jsx
@@ -1,7 +1,18 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 import Layout from "./Layout";
 
-export default function Home(props) {
-  return <Layout>Home</Layout>;
+export default function Home() {
+  useEffect(() => {
+    {
+      swal.fire({
+        title: "Welcome back",
+      });
+    }
+  }, []);
+  return (
+    <Layout>
+      <h1>Welcome to Amar Blog</h1>
+    </Layout>
+  );
 }
