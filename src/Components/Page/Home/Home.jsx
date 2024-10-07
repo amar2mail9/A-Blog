89f8dc434bfffac4bcacc9fef5511ad1fb4d9606
Amar Kumar <amar2mail9@gmail.com>
2024-10-07 19:08:07 +0530

    Add Seet alert

--- src/Components/Page/Home/Home.jsx
+++ src/Components/Page/Home/Home.jsx
@@ -1,18 +1,22 @@
-import React, { useEffect } from "react";
+import React from "react";
 
 import Layout from "./Layout";
+import { Button } from "react-bootstrap";
+import Swal from "sweetalert2";
 
 export default function Home() {
-  useEffect(() => {
-    {
-      swal.fire({
-        title: "Welcome back",
-      });
-    }
-  }, []);
   return (
     <Layout>
-      <h1>Welcome to Amar Blog helloo a everyone</h1>
+      <h1>
+        Welcome to Amar Blog helloo a everyone
+        <Button
+          onClick={() => {
+            Swal.fire("SweetAlert2 is working!");
+          }}
+        >
+          click me
+        </Button>
+      </h1>
     </Layout>
   );
 }
