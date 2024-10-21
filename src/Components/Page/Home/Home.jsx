5c707aefa57ca0b9e3bd44e06d49ee371697d78f
amar2mail9 <amar2mail9@gmail.com>
2024-10-21 22:38:18 +0530

    Redirects Coming soon add

--- src/Components/Page/Home/Home.jsx
+++ src/Components/Page/Home/Home.jsx
@@ -1,8 +1,7 @@
 import React from "react";
 import Layout from "./Layout";
 import { motion } from "framer-motion";
-import FeaturedPosts from "../FeatureAndBlog/FeaturedPosts";
-import HeroSection from "../../HeroSection/HeroSection";
+
 import Hero from "../HeroSection/Hero";
 
 export default function Home() {
@@ -10,9 +9,19 @@ export default function Home() {
     <Layout>
       {/* About */}
       <Hero />
-      <h1 className="text-center text-4xl mt-8 text-orange-500 animate-pulse font-semibold ">
-        Website Under Process...
-      </h1>
+      <motion.div
+      
+      initial={{ opacity: 0, scale: 0.8 }}
+      animate={{ opacity: 1, scale: 1 }}
+      transition={{ duration: 1, delay: 0.9 }}>
+        <h1 className="text-center text-4xl mt-8 text-orange-500 animate-pulse font-semibold ">
+          Website Under Process...
+        </h1>
+
+        <h1 className="text-center text-4xl mt-8 text-sky-500 animate-pulse font-semibold ">
+          More Information Coming Soon....
+        </h1>
+      </motion.div>
     </Layout>
   );
 }
