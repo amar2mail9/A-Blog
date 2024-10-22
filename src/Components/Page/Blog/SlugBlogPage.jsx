653cc1c15054db99bdc0ff8ec7f90e1734e41e38
amar2mail9 <amar2mail9@gmail.com>
2024-10-22 10:23:19 +0530

    fixed error 03

--- src/Components/Page/Blog/SlugBlogPage.jsx
+++ src/Components/Page/Blog/SlugBlogPage.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { useTechBlog } from "../../../App";
 import { Spinner } from "react-bootstrap";
@@ -7,10 +7,9 @@ import Layout from "../Home/Layout";
 function SlugBlogPage() {
   const { fetchData, individualBlog, isLoading } = useTechBlog();
   const { title } = useParams();
-  console.log(title);
 
   useEffect(() => {
-    fetchData.fetchIndividualBlog(title); // Fetch the individual blog post using the ID from params
+    fetchData.fetchIndividualBlog(title);
   }, [title]);
 
   return (
